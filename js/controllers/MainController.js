b5d5a1bce6263a161c2ed5213ae733759d6f99c9
SavourEarth <33814310+SavourEarth@users.noreply.github.com>
2017-11-30 00:45:04 +0100

    Switch to Angular

---
+++ js/controllers/MainController.js
@@ -0,0 +1,20 @@
+app.controller('MainController', ['$scope', function($scope) {
+  $scope.title = 'I want the hungry to be fed.';
+  $scope.facts=[
+    {background:'img/savourearth5.jpg',
+    text:'I want wars caused by drought to end.',
+  },
+  {background:'img/savourearth1.jpg',
+  text:'I want wars caused by drought to end.',
+},
+{background:'img/savourearth4.jpg',
+text:'I want climate change reversed.',
+},
+{background:'img/savourearth3.jpg',
+text:'I want rainforests to last for generations.',
+},
+{background:'img/savourearth2.jpg',
+text:'I want to remain active for all my life.',
+}
+]
+}]);
