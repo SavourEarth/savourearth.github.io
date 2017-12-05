383c1494826dc6ac88087cef84b2c93428bd629a
SavourEarth <33814310+SavourEarth@users.noreply.github.com>
2017-12-05 22:37:34 +0100

    routing test

--- js/app.js
+++ js/app.js
@@ -1,1 +1,12 @@
-var app = angular.module("myApp",[]);
+var app = angular.module("myApp", ["ngRoute"]);
+app.config(function($routeProvider) {
+  $routeProvider
+  .when("/", {
+    // templateUrl : "views/home.html",
+    // controller: "MainController"
+    template : "<h1>Banana</h1><p>Bananas contain around 75% water.</p>"
+  .when("/about", {
+      templateUrl : "views/about.html"
+    })
+  });
+});
--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,20 +1,26 @@
 app.controller('MainController', ['$scope', function($scope) {
-  $scope.title = 'I want the hungry to be fed.';
+  $scope.msg = "I love Bood";
   $scope.facts=[
-    {background:'img/savourearth5.jpg',
-    text:'I want wars caused by drought to end.',
-  },
-  {background:'img/savourearth1.jpg',
-  text:'I want wars caused by drought to end.',
-},
-{background:'img/savourearth4.jpg',
-text:'I want climate change reversed.',
-},
-{background:'img/savourearth3.jpg',
-text:'I want rainforests to last for generations.',
-},
-{background:'img/savourearth2.jpg',
-text:'I want to remain active for all my life.',
-}
-]
+    {
+      background:'img/savourearth5.jpg',
+      text:'I want the hungry to be fed.'
+      },
+    {
+      background:'img/savourearth1.jpg',
+      text:'I want wars caused by drought to end.'
+      },
+    {
+      background:'img/savourearth4.jpg',
+      text:'I want climate change reversed.'
+      },
+    {
+      background:'img/savourearth3.jpg',
+      text:'I want rainforests to last for generations.'
+      },
+    {
+      background:'img/savourearth2.jpg',
+      text:'I want to remain active for all my life.'
+    }
+  ]
+
 }]);
