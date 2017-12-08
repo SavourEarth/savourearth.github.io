b735a7c29d9028ae30765cb0f6a3196655e1980c
SavourEarth <33814310+SavourEarth@users.noreply.github.com>
2017-12-08 01:02:46 +0100

    proper angular 1.x

--- js/app.js
+++ js/app.js
@@ -1,12 +1,17 @@
-var app = angular.module("myApp", ["ngRoute"]);
+var app = angular.module("myApp", ["ngRoute","ngAnimate"]);
 app.config(function($routeProvider) {
+
   $routeProvider
   .when("/", {
-    // templateUrl : "views/home.html",
-    // controller: "MainController"
-    template : "<h1>Banana</h1><p>Bananas contain around 75% water.</p>"
+     controller: "MainController",
+     templateUrl : "views/home.html"
+
+    })
+    .when("/cat/:topic", {
+       controller: "FactController",
+       templateUrl : "views/facts.html"
+      })
   .when("/about", {
       templateUrl : "views/about.html"
-    })
   });
 });
---
+++ js/controllers/FactController.js
@@ -0,0 +1,41 @@
+app.controller('FactController', function($scope, $routeParams) {
+  $scope.topic=$routeParams.topic;
+  $scope.facts=[
+    {
+      topic:'hunger',
+      texts:
+        [
+          {text:'In 2016 The Vegan Society in partnership with Vegan Life Magazine commissioned research that found that there were over half a million vegans in Great Britain: three and a half times as many as estimated in 2006.'},
+          {text:'BIn 2016 The Vegan Society in partnership with Vegan Life Magazine commissioned research that found that there were over half a million vegans in Great Britain: three and a half times as many as estimated in 2006.'},
+          {text:'In 2016 The Vegan Society in partnership with Vegan Life Magazine commissioned research that found that there were over half a million vegans in Great Britain: three and a half times as many as estimated in 2006.'},
+          {text:'In 2016 The Vegan Society in partnership with Vegan Life Magazine commissioned research that found that there were over half a million vegans in Great Britain: three and a half times as many as estimated in 2006.'},
+          {text:'In 2016 The Vegan Society in partnership with Vegan Life Magazine commissioned research that found that there were over half a million vegans in Great Britain: three and a half times as many as estimated in 2006.'},
+          {text:'In 2016 The Vegan Society in partnership with Vegan Life Magazine commissioned research that found that there were over half a million vegans in Great Britain: three and a half times as many as estimated in 2006.'}
+        ]
+      },
+    {
+      topic:'drought',
+      texts:
+        [
+          {text:'At least 542,000 people in Britain are now following a vegan diet and never consume any animal products including meat, fish, milk, cheese, eggs and honey. This is a whopping increase since the last estimate of 150,000 ten years ago.'},
+          {text:'BAt least 542,000 people in Britain are now following a vegan diet and never consume any animal products including meat, fish, milk, cheese, eggs and honey. This is a whopping increase since the last estimate of 150,000 ten years ago.'}
+        ]
+      },
+    {
+      topic:'climate',
+      texts:
+        [
+          {text:'The Vegan Society runs a mentoring scheme to help people who would like to become vegan called the Vegan Pledge. The number of Pledgers has doubled in the last year, with over 1000 people taking the Pledge each month.'},
+          {text:'BThe Vegan Society runs a mentoring scheme to help people who would like to become vegan called the Vegan Pledge. The number of Pledgers has doubled in the last year, with over 1000 people taking the Pledge each month.'}
+        ]
+      }
+  ]
+
+});
+
+
+
+
+
+rainforests
+active
