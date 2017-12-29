697009f3781d3f6edea7ff9cf7f6ba016278883a
SavourEarth <33814310+SavourEarth@users.noreply.github.com>
2017-12-29 16:35:23 +0100

    big update with all levels

--- js/app.js
+++ js/app.js
@@ -11,7 +11,67 @@ app.config(function($routeProvider) {
        controller: "FactController",
        templateUrl : "views/facts.html"
       })
+      .when("/:topic/:subtopic", {
+         controller: "DetailController",
+         templateUrl : "views/detail.html"
+        })
   .when("/about", {
       templateUrl : "views/about.html"
   });
+});
+
+app.run(function($rootScope) {
+    $rootScope.teasers = [
+      {
+        background:'img/savourearth5.jpg',
+        text:'I want the hungry to be fed.',
+        topic:'hunger'
+        },
+      {
+        background:'img/savourearth1.jpg',
+        text:'I want wars caused by drought to end.',
+        topic:'drought'
+        },
+      {
+        background:'img/savourearth4.jpg',
+        text:'I want climate change reversed.',
+        topic:'climate'
+        },
+      {
+        background:'img/savourearth3.jpg',
+        text:'I want rainforests to last for generations.',
+        topic:'rainforests'
+        },
+      {
+        background:'img/savourearth2.jpg',
+        text:'I want to remain active for all my life.',
+        topic:'active'
+      }
+    ];
+    $rootScope.subtopics=[
+      {
+        topic:'hunger',
+        subtopic:
+          [
+            {
+              title:'Cow Question',
+              link:'cows'
+            },
+            {
+              title:'Pig Question',
+              link:'pigs'
+            }
+          ]
+        },
+      {
+        topic:'drought',
+        subtopic:
+          [
+            {
+              title:'Question 1',
+              link:'q1'
+            }
+          ]
+        }
+    ]
 });
--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,30 +1,4 @@
 app.controller('MainController', function($scope) {
-  $scope.facts=[
-    {
-      background:'img/savourearth5.jpg',
-      text:'I want the hungry to be fed.',
-      topic:'hunger'
-      },
-    {
-      background:'img/savourearth1.jpg',
-      text:'I want wars caused by drought to end.',
-      topic:'drought'
-      },
-    {
-      background:'img/savourearth4.jpg',
-      text:'I want climate change reversed.',
-      topic:'climate'
-      },
-    {
-      background:'img/savourearth3.jpg',
-      text:'I want rainforests to last for generations.',
-      topic:'rainforests'
-      },
-    {
-      background:'img/savourearth2.jpg',
-      text:'I want to remain active for all my life.',
-      topic:'active'
-    }
-  ]
+
 
 });
